19b477ff8b86626e43b5778a22591ce8523ce70f
zhangwei <52408195+zhang-alt@users.noreply.github.com>
2022-04-30 09:55:39 +0800

    Update main.py

--- static_analysis/main.py
+++ static_analysis/main.py
@@ -6,6 +6,7 @@ from processing_generated_data import processing_generated_data
 from static_analysis_phase import static_analysis_phase
 
 def run():
+    
     static_analysis_phase()
     processing_generated_data()
 
